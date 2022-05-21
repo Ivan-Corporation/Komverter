cf06a3768ab81373405e14ab7e3335d81951b29c
Ivan <hammer.99.ivan@gmail.com>
2022-05-21 20:23:23 +0700

    front + url

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Komverter
+
+### Heroku problem fix
+git subtree push --prefix sub-folder heroku master
+ex:- git subtree push --prefix server heroku master
--- komverter_frontend/src/components/Header/Header.js
+++ komverter_frontend/src/components/Header/Header.js
@@ -1,99 +1,59 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
-import logo from '../../images/logo.png';
-import Container from '../Container';
-import SearchBar from '../SearchBar';
-import IconButton from '../IconButton';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CottageIcon from '@mui/icons-material/Cottage';
+import { RepositoryMetrics } from 'repository-metrics';
+import { motion } from 'framer-motion/dist/framer-motion'
 
-const Header = ({ withSearchBar, searchTerm, onChange, onSubmit }) => {
-  // withSearchBar - if false the search bar is alway hidden
-  // showSearchBar = shows/hides the search bar on mobile devices
-  // showSearchBar === true: the logo and home icon are hidden too
-  const [showSearchBar, setShowSearchBar] = useState(false);
-  const header = useRef(null);
+const Header = () => {
 
-  // eslint-disable-next-line consistent-return
-  useEffect(() => {
-    if (showSearchBar) {
-      const hideSearchBar = (e) => {
-        if (!header.current.contains(e.target)) {
-          setShowSearchBar(false);
-        }
-      };
 
-      document.addEventListener('click', hideSearchBar);
-
-      return () => document.removeEventListener('click', hideSearchBar);
-    }
-  }, [showSearchBar]);
-
-  const handleSubmit = (e) => {
-    onSubmit(e);
-    if (showSearchBar) {
-      setShowSearchBar(false);
-    }
-  };
 
   return (
-    <header ref={header} className={styles.header}>
-      <Container>
-        <div className={styles.headerContent}>
-          {!showSearchBar && (
-            <Link to="/">
-              <img src={logo} alt="logo" />
-            </Link>
-          )}
-          <div className={styles.headerContentRight}>
-            {withSearchBar && (
-              <div className={styles.searchBarContainer}>
-                <IconButton
-                  title="Hide search bar"
-                  className={showSearchBar ? styles.closeButton : styles.displayNone}
-                  onClick={() => {
-                    setShowSearchBar(false);
-                  }}
-                  icon="times"
-                  type="button"
-                  color="primary"
-                />
-                <SearchBar
-                  searchTerm={searchTerm}
-                  onSubmit={handleSubmit}
-                  onChange={onChange}
-                  className={showSearchBar ? '' : styles.searchBarDesktop}
-                />
-                <IconButton
-                  title="Search"
-                  className={showSearchBar ? styles.displayNone : styles.searchButton}
-                  onClick={() => {
-                    setShowSearchBar(true);
-                  }}
-                  icon="search"
-                  type="button"
-                  color="primary"
-                />
-              </div>
-            )}
-            {!showSearchBar && (
-              <Link to="/" title="Go to homepage">
-                <FontAwesomeIcon icon="home" size="lg" />
-              </Link>
-            )}
-          </div>
-        </div>
-      </Container>
+    <header className={styles.header}>
+      <motion.div
+                initial={{ opacity: 0, y: -180 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    ease: "easeInOut",
+                    duration: 1,
+                    delay: 0.2,
+                }}
+                >
+       <AppBar position="static" style={{backgroundColor:'darkorange'}}>
+       <Toolbar>
+         <Link to='/' >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            
+          >
+            
+            <CottageIcon  sx={{ fontSize: 30 }} style={{color:'white'}}/>
+          </IconButton>
+          </Link>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            <b>Komverter</b>
+          </Typography>
+          
+          <RepositoryMetrics 
+            owner='Ivan-Corporation' 
+            repo='Komverter' 
+            theme='light' 
+            />
+        </Toolbar>
+      </AppBar>
+      </motion.div>
     </header>
   );
 };
 
-Header.propTypes = {
-  withSearchBar: PropTypes.bool.isRequired,
-  searchTerm: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
 
 export default Header;
