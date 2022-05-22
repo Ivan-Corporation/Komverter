7d8dd90d728c1783beb3044af252b4cab16ef275
Ivan <hammer.99.ivan@gmail.com>
2022-05-23 00:43:22 +0700

    d

--- komverter_backend/index.js
+++ komverter_backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 app.use(cors())
 
 
-
+// check official library code
 app.get(`/download/:${downloadRouter}`, function (req, res, next) {
   res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
