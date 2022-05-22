043eaf66c389e02eaa72e2f2fda910f6b3117f1b
Ivan <hammer.99.ivan@gmail.com>
2022-05-22 23:52:40 +0700

    cors

--- komverter_backend/index.js
+++ komverter_backend/index.js
@@ -7,18 +7,14 @@ const app = express();
 const PORT = process.env.PORT;
 // const PORT = process.env.PORT || 4000;
 
-app.use(
-  cors({
-    origin: ['https://komverter.vercel.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-  })
-);
+
+
 
 
 
 app.use("/download", downloadRouter);
+app.use(cors());
+
 
 app.listen(PORT, () => {
   console.log(`Server is live at port ${PORT}`);
