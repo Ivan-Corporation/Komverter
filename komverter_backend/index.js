4700841cdba3be185ea8d3d535eaab18fba9c0aa
Ivan <hammer.99.ivan@gmail.com>
2022-05-22 23:26:45 +0700

    cors fix

--- komverter_backend/index.js
+++ komverter_backend/index.js
@@ -5,8 +5,16 @@ const downloadRouter = require("./routes/download");
 
 const app = express();
 const PORT = process.env.PORT;
+// const PORT = process.env.PORT || 4000;
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 
 app.use("/download", downloadRouter);
 
