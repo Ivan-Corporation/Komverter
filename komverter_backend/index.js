73a648d6070de63685937b03785a9c645e3bcf0d
Ivan <hammer.99.ivan@gmail.com>
2022-05-23 10:42:14 +0700

    heroku 400

--- komverter_backend/index.js
+++ komverter_backend/index.js
@@ -1,30 +1,13 @@
-// YouTube MP3 download and converter API
 const express = require("express");
 const cors = require("cors");
 const downloadRouter = require("./routes/download");
 
 const app = express();
-// const PORT = process.env.PORT;
 const PORT = process.env.PORT || 4000;
 
-
-
-
-
-// F
-app.use(cors())
-
-
-// check official library code
-// app.get(`/download/:${downloadRouter}`, function (req, res, next) {
-//   res.json({ msg: 'This is CORS-enabled for all origins!' })
-// })
-
+app.use(cors());
 
 app.use("/download", downloadRouter);
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is live at port ${PORT}`);
--- komverter_backend/routes/download.js
+++ komverter_backend/routes/download.js
@@ -18,10 +18,7 @@ downloadRouter.get("/:videoId", async (req, res, next) => {
 
     res.set({
       "Content-Disposition": `attachment; filename="${fileName}.mp3"`,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
       "Access-Control-Expose-Headers": "Content-Disposition",
-      "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, PATCH, OPTIONS",
       "Content-Type": "audio/mpeg",
     });
 
