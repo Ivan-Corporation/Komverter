b4618b33c4c947556a09ded3e7ba5557f8f54b13
Ivan <hammer.99.ivan@gmail.com>
2022-05-23 10:19:26 +0700

    cors

--- komverter_backend/index.js
+++ komverter_backend/index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const downloadRouter = require("./routes/download");
 
 const app = express();
-const PORT = process.env.PORT;
-// const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 
 
@@ -16,9 +16,9 @@ app.use(cors())
 
 
 // check official library code
-app.get(`/download/:${downloadRouter}`, function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
+// app.get(`/download/:${downloadRouter}`, function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for all origins!' })
+// })
 
 
 app.use("/download", downloadRouter);
